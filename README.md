c41a6b11359307113c9c570fafbd2bd3517c65d7
Kirill Markin <markinkirill@gmail.com>
2023-06-08 09:50:43 +0200

    baseline

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # split-documents-upload-to-pinecone
+
 Divide documents and upload text segments to Pinecone with the script.
+
+## Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/kirill-markin/split-documents-upload-to-pinecone.git
+    ```
+
+2. Install the required packages:
+
+    ```bash
+    cd split-documents-upload-to-pinecone
+    pip install -r requirements.txt
+    ```
+
+3. Set up environment variables in a `.env` file in the project root based on the `.env.example` file:
+
+    ```ini
+    OPENAI_API_KEY=your_openai_api_key
+    PINECONE_API_KEY=your_pinecone_api_key
+    PINECONE_ENVIRONMENT=your_pinecone_environment
+    PINECONE_INDEX_NAME=your_index_name
+    ```
+
+    Replace `your_pinecone_api_key` and `your_pinecone_environment` with your actual values.
+
+    `your_pinecone_environment` — the name of the Pinecone environment you want to use. For example `us-central1-gcp`.
+    `your_index_name` — the name of the index you want to create. You can use any name.
+
+## Usage
+
+Add documents to the `data` folder. It can be a single file or multiple files in inner folders. The script will process all files `*.md` in the folder.
+
+Run the script:
+
+```bash
+python3 main.py
+```
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ main.py
@@ -0,0 +1,102 @@
+import os
+import datetime
+
+import pinecone
+from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Load documents
+
+directory = './data'
+
+def load_docs(directory):
+  loader = DirectoryLoader(directory, glob="**/*.md", loader_cls=TextLoader)
+  documents = loader.load()
+  return documents
+
+documents = load_docs(directory)
+print(f"Number of documents: {len(documents)}")
+
+
+# Split documents into chunks
+
+def split_docs(documents, chunk_size=1000, chunk_overlap=20):
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+  docs = text_splitter.split_documents(documents)
+  return docs
+
+docs = split_docs(documents)
+print(f"Number of splited documents: {len(docs)}")
+
+
+# Save result in a file to check the result manually
+
+# create folder with logs if not exists
+logs_dir = './logs'
+if not os.path.exists(logs_dir):
+  os.makedirs(logs_dir)
+
+now = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+with open(f'{logs_dir}/{now}-temp.md', 'w') as f:
+  for d in docs:
+    f.write(d.page_content)
+    f.write('\n\n\n!--------------------------------------------\n\n\n')
+
+
+# Prepare embeddings
+
+embeddings = OpenAIEmbeddings()
+
+query_result = embeddings.embed_query("Hello world")
+dimension = len(query_result)
+print(f"Dimension in embeddings: {dimension}")
+
+
+# Pinecone init
+
+pinecone.init(
+    api_key=os.getenv("PINECONE_API_KEY"),
+    environment=os.environ.get('PINECONE_ENVIRONMENT'),
+)
+
+index_name = os.environ.get('PINECONE_INDEX_NAME')
+
+
+# Recreate the index
+# Can take 15 minutes or more
+
+NotFoundException = pinecone.exceptions.NotFoundException
+
+try:
+    pinecone.delete_index(name=index_name)
+    print(f"Index '{index_name}' deleted.")
+except NotFoundException:
+    print(f"Index '{index_name}' not found. Skipping deletion.")
+
+pinecone.create_index(
+    name=index_name,
+    dimension=dimension,
+    metric="cosine",
+)
+
+print(f"pinecone.describe_index(index_name):, {pinecone.describe_index(index_name)}")
+
+
+# Upload the embeddings to the index
+# Can take 10 minutes or more
+
+index = Pinecone.from_documents(docs, embeddings, index_name=index_name) # create and upload
+# index = Pinecone.from_existing_index(index_name, embeddings) # connect to existing index
+
+
+query = "test"
+k = 2
+print(index.similarity_search_with_score(query, k=k))
